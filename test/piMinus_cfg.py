fadc64c4e06463cbe0f2ff628e889c25c57354b6
jjbuchanan <jjbuchanan42@gmail.com>
2017-06-09 23:03:48 +0200

    Updated for CMSSW 9_2_0

--- test/piMinus_cfg.py
+++ test/piMinus_cfg.py
@@ -2,23 +2,27 @@ import os
 import FWCore.ParameterSet.Config as cms
 import EventFilter.L1TXRawToDigi.util as util
 
-process = cms.Process("HFCALIB")
+## Define the process
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process("HFCALIB",eras.Run2_2017)
 
-## Import of standard configurations
-process.load('FWCore.MessageService.MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+## Load all kinds of stuff
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
-process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi') # New
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff') # New
+process.load('SimGeneral.MixingModule.mixNoPU_cfi') # New
+# process.load('Configuration.StandardSequences.SimL1EmulatorRepack_FullMC_cff') # New; results in error whereby 0 digis are in collections
+process.load('Configuration.StandardSequences.MagneticField_cff')
+# Alternative if needed:
+# process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.RawToDigi_cff')
-
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
-process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_v6'
-
-process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.load('Configuration.Geometry.GeometryRecoDB_cff')
+# Alternatives if needed:
+# process.load("Configuration.Geometry.GeometryExtended2017Reco_cff")
+# process.load("Configuration.Geometry.GeometryExtended2017Plan1Reco_cff")
 
 process.source = cms.Source("PoolSource",
         fileNames = cms.untracked.vstring(
@@ -26,31 +30,45 @@ process.source = cms.Source("PoolSource",
 #'/store/mc/RunIIFall15DR76/SinglePiMinus_E1to1000_Eta5p2_13TeV_FlatRandom/GEN-SIM-RAW/25nsNoPUZsecalNzshcalRaw_76X_mcRun2_asymptotic_2016EcalTune_30fb_v1-v1/60000/105B3BC2-2BED-E511-A51F-008CFA56D6F4.root'
         )
 )
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
-process.load("Configuration.Geometry.GeometryExtended2016Reco_cff")
-# To get CaloTPGTranscoder
+## GlobalTag
+from Configuration.AlCa.GlobalTag import GlobalTag
+# Get updated tag from https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions
+process.GlobalTag = GlobalTag(process.GlobalTag,'90X_upgrade2017_realistic_v20','')
+# Get updated tag from https://twiki.cern.ch/twiki/bin/view/CMS/HcalLUTCorrsTags2009
+process.GlobalTag.toGet = cms.VPSet(
+  cms.PSet(record = cms.string("HcalLUTCorrsRcd"),
+           tag = cms.string("HcalLUTCorrs_2017plan1_v2.0_mc"),
+           connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
+          )
+)
+
+## To get the CaloTPGTranscoder, which decodes the HCAL compression LUT
 process.load("SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff")
+# From an email by Aleko: "For simulation you want LUTGenerationMode=True"
+process.HcalTPGCoderULUT.LUTGenerationMode = cms.bool(True)
 
-
-process.load("Configuration.Geometry.GeometryExtended2016Reco_cff")
-
+from CondCore.DBCommon.CondDBSetup_cfi import *
 process.es_pool = cms.ESSource("PoolDBESSource",
-     process.CondDBSetup,
+     CondDBSetup,
      timetype = cms.string('runnumber'),
      toGet = cms.VPSet(
          cms.PSet(record = cms.string("HcalLutMetadataRcd"),
-             tag = cms.string("HcalLutMetadata_HFTP_1x1")
+             # Get updated tag from https://twiki.cern.ch/twiki/bin/view/CMS/HcalLutMetadataTags2011
+             tag = cms.string("HcalLutMetadata_2017plan1_v3.0_mc")
              ),
          cms.PSet(record = cms.string("HcalElectronicsMapRcd"),
-             tag = cms.string("HcalElectronicsMap_HFTP_1x1")
+             # Get updated tag from https://twiki.cern.ch/twiki/bin/view/CMS/HcalElectronicsMapTags2009
+             tag = cms.string("HcalElectronicsMap_2017plan1_v3.0_mc")
              )
          ),
      connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
      authenticationMethod = cms.untracked.uint32(0)
      )
 process.es_prefer_es_pool = cms.ESPrefer( "PoolDBESSource", "es_pool" )
-
-
 
 process.TFileService = cms.Service("TFileService",
         closeFileFast = cms.untracked.bool(True),
@@ -59,20 +77,9 @@ process.TFileService = cms.Service("TFileService",
 
 process.tree = cms.EDAnalyzer("PionCalibrations",
         triggerPrimitives = cms.InputTag("simHcalTriggerPrimitiveDigis","","HFCALIB"),
-        #triggerPrimitives = cms.InputTag("hcalDigis"),
-        #eTriggerPrimitives = cms.InputTag("simEcalTriggerPrimitiveDigis"),
         eTriggerPrimitives = cms.InputTag("ecalDigis:EcalTriggerPrimitives"),
         genSrc = cms.InputTag("genParticles"),
         doClosure = cms.untracked.bool(False)
 )
 
-
-#process.p = cms.Path(process.hcalDigis * process.ecalDigis * process.tree )
-#process.p = cms.Path(process.hcalDigis * process.simHcalTriggerPrimitiveDigis * process.ecalDigis * process.tree )
-#process.p = cms.Path(process.l1tCaloLayer1Digis * process.tree)
 process.p = cms.Path(process.hcalDigis * process.ecalDigis * process.simHcalTriggerPrimitiveDigis *process.tree)
-
-#process.p = cms.Path(process.simHcalTriggerPrimitiveDigis * process.l1tCaloLayer1Digis* process.tree)
-
-#process.p = cms.Path(process.l1tCaloLayer1Digis*process.simCaloStage2Layer1Digis*process.rctDigis * process.tree)
-#print process.dumpPython()
